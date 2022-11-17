3c53f2eb1d119a4188ce46a87307bc46d387a883
Ashish <ashish08271@gmail.com>
2022-11-17 17:18:57 +0530

    ARCNA

---
+++ app/src/main/java/com/ashish/a7minutesworkout/ExerciseActivity.kt
@@ -0,0 +1,27 @@
+package com.ashish.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ashish.a7minutesworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+
+    private var binding : ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener{
+            onBackPressed()
+        }
+
+    }
+}
